b39d8f798db1726445a051d7ad3f487b2617a61a
AshAbb1512 <167420021+AshAbb1512@users.noreply.github.com>
2024-04-18 11:49:40 +0000

    Instrument AWS X-Ray for Flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,7 +32,7 @@ xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 
 xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 #HoneyComb ------------------------
 # Initialize tracing and an exporter that can send data to Honeycomb
